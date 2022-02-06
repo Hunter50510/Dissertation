5fb5609a2479544224db5a3e531e05cf1f19b7f3
Hunter50510 <94972738+Hunter50510@users.noreply.github.com>
2022-02-06 14:46:06 +0000

    Working with correct graphs

--- diss_test_running.m
+++ diss_test_running.m
@@ -1,27 +1,41 @@
 function diss_test_running
 tic
 fprintf('Minimum speed difference achived =')
-sat = 1000;
+sat_start = 4000;
+sat = sat_start;
+sat_max = 4100;
+sat_1 = sat;
+sat_step = 100;
+rot_start = 0;
+rot_step = 0.5;
+rot_max = 3;
 i = 1;
 j = 0;
 min_store = zeros(length(i));
 max_store = zeros(length(i));
-while sat <= 2000
+while sat <= sat_max
     j = j + 1;
-    i = i + 1;
     k = 1;
-    rot = 1;
-while k <= 5
+    rot = rot_start;
+while rot <= rot_max
     [min,max] = diss_test_function(rot,sat);
     fprintf('%gkm/s\n',min)
     min_store(k,j) = min;
     max_store(k,j) = max;
-    rot = rot + 1;
+    rot = rot + rot_step;
     k = k + 1;
 end
 
-sat = sat + 1000;
+sat = sat + sat_step;
 
+end
+
+while i <= j
+    min_total = mink(min_store(1:end,i),2);
+    fprintf('\nThe two smallest values for %gkm cable are:\n%gkm/s\n%gkm/s\n' ...
+        ,sat_1,min_total)
+    sat_1 = sat_1 + sat_step;
+    i = i + 1;
 end
 
 figure(2)
@@ -29,14 +43,21 @@ bar3(min_store)
 title('Minimum speeds at different lengths and rotations')
 ylabel('Number of rotations per orbit')
 zlabel('Minimum speed (km/s)')
-xlabel('Length (\times1000km)')
+xlabel('Length (\times100km)')
+set(gca,'YTick',0:1:k)
+set(gca,'YTickLabel',rot_start-rot_step:rot_step:rot_max)
+set(gca,'XTick',0:1:j)
+set(gca,'XTickLabel',sat_start-sat_step:sat_step:sat_max)
 
 figure(3)
 bar3(max_store)
 title('Maximum speeds at different lengths and rotations')
 ylabel('Number of rotations per orbit')
 zlabel('Minimum speed (km/s)')
-xlabel('Length (\times1000km)')
-
+xlabel('Length (\times100km)')
+set(gca,'YTick',0:1:k)
+set(gca,'YTickLabel',rot_start-rot_step:rot_step:rot_max)
+set(gca,'XTick',0:1:j)
+set(gca,'XTickLabel',sat_start-sat_step:sat_step:sat_max)
 toc
 end
